1be11b4c5860ae8f6071d4633e269e68f2a67acd
TrinhThaiHoang1939 <trinhhoang280205@gmail.com>
2025-05-26 10:18:20 +0700

    Add files via upload

---
+++ src/java/entity/Material.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class Material {
+    private String name;
+    private String code;
+    private String unit;
+    private double price;
+
+    // Constructor
+    public Material(String name, String code, String unit, double price) {
+        this.name = name;
+        this.code = code;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    // Getters and Setters...
+}
