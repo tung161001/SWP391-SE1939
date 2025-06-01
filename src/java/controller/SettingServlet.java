fdbe89b4d1280e002f63571e392284d9e2d73e3d
TrinhThaiHoang1939 <trinhhoang280205@gmail.com>
2025-06-01 10:33:38 +0700

    Add files via upload

---
+++ src/java/controller/SettingServlet.java
@@ -0,0 +1,31 @@
+package controller;
+
+import dao.SettingDAO;
+import entity.Setting;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import entity.Setting;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/settings")
+public class SettingServlet extends HttpServlet {
+
+    private SettingDAO settingDAO;
+
+    @Override
+    public void init() {
+        settingDAO = new SettingDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Setting> settings = settingDAO.getAll();
+        request.setAttribute("settings", settings);
+        request.getRequestDispatcher("jsp/settingList.jsp").forward(request, response);
+    }
+}
