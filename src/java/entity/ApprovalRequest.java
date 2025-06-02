9b39d31b65d017ccbb26da47693af109c9dc0505
anhdoan12345 <doanhche180633@fpt.edu.vn>
2025-06-02 12:58:47 +0700

    Add files via upload

--- src/java/entity/ApprovalRequest.java
+++ src/java/entity/ApprovalRequest.java
@@ -9,5 +9,39 @@ package entity;
  * @author Doan PC
  */
 public class ApprovalRequest {
-    
+
+    private int id;
+    private String requestType;
+    private String approvalStatus;
+    private String comments;
+
+    // Constructors
+    public ApprovalRequest(int id, String requestType, String approvalStatus, String comments) {
+        this.id = id;
+        this.requestType = requestType;
+        this.approvalStatus = approvalStatus;
+        this.comments = comments;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public String getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    // Setters
+    public void setApprovalStatus(String approvalStatus) {
+        this.approvalStatus = approvalStatus;
+    }
 }
